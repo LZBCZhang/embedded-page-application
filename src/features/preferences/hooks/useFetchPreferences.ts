6308d2b069c5cd8245f1857eb1d204f723bcffc2
LZBCZhang <35249876+LZBCZhang@users.noreply.github.com>
2026-06-07 22:36:21 +0200

    Create useFetchPreferences.ts

---
+++ src/features/preferences/hooks/useFetchPreferences.ts
@@ -0,0 +1,8 @@
+import { useMutation } from '@tanstack/react-query';
+import { fetchUserPreferences } from '../api/preferences.api';
+import type { FetchUserPreferencesRequest, UserPreferencesResponse } from '../types/preferences.types';
+
+export const useFetchPreferences = () =>
+    useMutation<UserPreferencesResponse, unknown, FetchUserPreferencesRequest>({
+        mutationFn: fetchUserPreferences
+    });
