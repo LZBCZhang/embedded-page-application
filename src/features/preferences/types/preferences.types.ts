93e13b29556f30ee655dddb88b5eaed44b4b4920
LZBCZhang <35249876+LZBCZhang@users.noreply.github.com>
2026-06-07 22:35:28 +0200

    Update preferences.types.ts

--- src/features/preferences/types/preferences.types.ts
+++ src/features/preferences/types/preferences.types.ts
@@ -1,16 +1,69 @@
-export interface ConsentPreference {
+export type ConsentStatus = 'WITHDRAWN' | 'CONFIRMED';
+
+export interface NotificationOption {
+  id: string;
+  type: string;
+  consented: boolean;
+}
+
+export interface CommunicationPreference {
+  id: string;
+  name: string;
+  type: string;
+  options: NotificationOption[];
+}
+
+export interface OtherPreference {
+  id: string;
+  name: string;
+  type: string;
+  options: NotificationOption[];
+}
+
+export interface Purpose {
   id: string;
   label: string;
   description: string;
-  enabled: boolean;
-  required: boolean;
+  status: string;
+  version: number;
+  purposeType: string;
+  communicationPreferences: CommunicationPreference[];
+  otherPreferences: OtherPreference[];
 }
 
-export interface UpdatePreferencePayload {
-  id: string;
-  enabled: boolean;
+export interface FetchUserPreferencesRequest {
+  userId: string;
+  collectionPointId: string;
 }
 
-export interface PreferencesResponse {
-  preferences: ConsentPreference[];
+export interface UserPreferencesResponse {
+  meta: {
+    correlationId: string;
+  },
+  data: Purpose[]
+}
+
+export interface AdditionalInfo {
+  email: string;
+  company: string;
+  mobile: string;
+}
+
+export interface PurposeUpdate {
+  purposeId: string;
+  status: ConsentStatus;
+  communicationPreferences: string[];
+}
+
+export interface UpdateUserPreferencesRequest {
+  userId: string;
+  collectionPointId: string;
+  source: string;
+  purposes: PurposeUpdate[];
+  additionalInfo?: AdditionalInfo;
+}
+
+export interface CommunicationType {
+  type: string;
+  name: string;
 }
